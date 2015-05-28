fb3af2b3fdb503ce1ce07c1c1333aaf9e2dcf7fa
Miguel Uribe <migueluribe@Miguels-Mac.local>
2015-05-28 11:16:42 -0700

    Initial Commit

---
+++ AdaptiveWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdaptiveWeather
+//
+//  Created by Miguel Uribe on 28/05/15.
+//  Copyright (c) 2015 Miguel Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdaptiveWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveWeather
+//
+//  Created by Miguel Uribe on 28/05/15.
+//  Copyright (c) 2015 Miguel Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
